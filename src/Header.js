450fd1fa51082218e1dd0223bf65ca15197db00c
Necron <necron3299@gmail.com>
2025-01-12 23:12:15 +0100

    Header improvements

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,14 @@ import './Header.css';
 
 function Header() {
     return (
-        <header>
-            <div className="siteNameDiv">
+        <header className="HeaderSiteNameContainer">
+            <div className="DivSiteName">
             Welcome to the Film Renting Site
             </div>
             <nav>
-                
+                <a>shop</a>
+                <a>support</a>
+                <a>cart</a>
             </nav>
         </header>
     )
